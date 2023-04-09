5d0857f0870f99c78d945bdfa5e0c256415dcf6b
Birlutiu Claudiu <birlutiuclaudiuc@gmail.com>
2023-04-09 08:19:06 -0400

    add sarcina 6

--- L05/Birlutiu_Claudiu_Lab05/Birlutiu_Claudiu_Cod/sarcina6_birlutiu/sarcina6.3_birlutiu/sample_code.py
+++ L05/Birlutiu_Claudiu_Lab05/Birlutiu_Claudiu_Cod/sarcina6_birlutiu/sarcina6.3_birlutiu/sample_code.py
@@ -5,26 +5,23 @@ def xor(first, second):
    return bytearray(x^y for x,y in zip(first, second))
 
 
-IV_1 = "cdd58a1dc01a104cbaf4bac83e596c34"   #reprezinta primul IV cu care Bob a codificat mesajul 
-CT_1 = "7f99ac57d9ff9f55968386b38e9590b9"   #mesajul codificat cu ajutorul IV_1
-IV_2 = "5b369a33c01a104cbaf4bac83e596c34"   #IV -ul urmator ca predictie
-
+IV_1 = "b8c1226d0517a72f71acf59c98ff9076"   #reprezinta primul IV cu care Bob a codificat mesajul 
+CT_1 = "90e620623fa06434e0f71401909d9192"   #mesajul codificat cu ajutorul IV_1
+IV_2 = "5b30bedf0517a72f71acf59c98ff9076"   #IV -ul urmator; predictibil
 #ideea e sa ghicim ce a zis BOB astfel ca vom prezice daca a spus Yes
-Pred_1 = "No"
+Pred_1 = "Yes" #valoare presupusa
 
 # Convert ascii string to bytearray
 P1 = bytes(Pred_1, 'utf-8')                #obtinerea de bytes din sirul de caractere
 
 # Convert hex string to bytearray
-IV_1 = bytearray.fromhex(IV_1)                               #obtinerea sirului de bytes din valorile hexa
-IV_2 = bytearray.fromhex(IV_2)
-CT_1 =  bytearray.fromhex(CT_1)
+IV_1_bytes = bytearray.fromhex(IV_1)             #obtinerea sirului de bytes din valorile hexa
+IV_2_bytes = bytearray.fromhex(IV_2)
+CT_1_bytes =  bytearray.fromhex(CT_1)
 #in prima faza vom face un XOR intre predictie si IV_1 folosit de catre Bob
-K = xor(P1, IV_1)
+K = xor(P1, IV_1_bytes)
 #verificam ce am obtine daca raspunsul al doilea al lui Bob este Yes sau No
 #facadn XOr intre rezultatul anterior si IV_2 predictibil
-CT_2 = xor(K, IV_2)
-#CT_2 =CT_2.hex()  #convertirea la hex
-print(CT_1.hex())
-print(CT_2.hex())
-print(P1.hex())
+CT_2 = xor(K, IV_2_bytes)
+print("Rezultat: ", CT_2.hex())
+
